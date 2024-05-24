6a2cefb6573a9e5f3d25a060de4b6f370d96f54a
Molochnyy <153619807+Molochnyy@users.noreply.github.com>
2024-05-24 14:40:00 +0300

    /

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".feedback-form");
+    const formData = {
+        email: "",
+        message: ""
+    };
+
+    const storedFormData = localStorage.getItem("feedback-form-state");
+    if (storedFormData) {
+        const parsedFormData = JSON.parse(storedFormData);
+        formData.email = parsedFormData.email;
+        formData.message = parsedFormData.message;
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    }
+
+    form.addEventListener('input', function (event) {
+        const fieldName = event.target.name;
+        const fieldValue = event.target.value.trim();
+        formData[fieldName] = fieldValue;
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    });
+
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        if (formData.email === "" || formData.message === "") {
+            alert("Fill please all fields");
+            return;
+        }
+        console.log(formData);
+        localStorage.removeItem("feedback-form-state");
+        form.reset();
+    });
+});
